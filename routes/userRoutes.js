b45c88cd6e41661ec8be2ceea9937a917e8f0595
AakashShahi <shahiaakash112@gmail.com>
2025-06-12 21:47:44 +0545

    route for register and login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require("express")
 const router = express.Router()
 const userController = require("../controllers/userController")
 
+//route for register
 router.post(
     "/register",
     userController.regiterUser
 )
 
+//route for login
 router.post(
     "/login",
     userController.loginUser

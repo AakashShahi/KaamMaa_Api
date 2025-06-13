b47f6b70652d2321c7fa027a8bd2a618a8d55b00
AakashShahi <shahiaakash112@gmail.com>
2025-06-13 19:08:27 +0545

    Job model made

---
+++ models/Job.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+//Jobs schema
+const JobSchema = mongoose.Schema(
+    {
+        postedBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        assignedTo: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: 'ProfessionCategory'
+        },
+        description: String,
+        location: String,
+        time: Date,
+        status: {
+            type: String,
+            enum: ['open', 'in-progress', 'done', 'failed'],
+            default: 'open'
+        },
+        review: {
+            rating: Number,
+            comment: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
